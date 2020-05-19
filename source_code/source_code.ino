ef6ba94730507ae74b0db75c1ff8d0719c87330e
n0crush <windyht0810@gmail.com>
2020-05-19 10:13:49 +0700

    change folder

---
+++ source_code/source_code.ino
@@ -0,0 +1,106 @@
+#include <LiquidCrystal.h>
+#include <string.h>
+
+void welcome(char[], char[]);
+void show_setup();
+void show_detail();
+
+// function prototype
+//---------------------------------
+ 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+const float H_MAX = 3.3;                          // adj
+const int MIN_ACT = 40;
+int button = 7 ;                                  // get digital input from pin 7st
+
+float h_input = 1.65;                             // temporory input
+int humi =  (h_input*100)/H_MAX;                  // humidity
+
+
+char x0[] = "-.- CCK12DT1 -.-";
+char x1[] = " Auto Watering ";
+char sts[] = "OFF";
+int hh=23;
+int mm=59;
+//----------------------------------
+
+void setup() {
+  pinMode(button, INPUT);
+  //
+
+  lcd.begin(16, 2);                               // declare LCD 1602
+  Serial.begin(9600);                             // debugging
+  show_setup();
+
+}
+//----------------------------------
+int getButtonStatus(){
+  //
+  short buttonStatus;
+  buttonStatus = digitalRead(button);
+  return buttonStatus;
+}
+
+void show_setup(){
+  lcd.setCursor(0, 0);
+  lcd.print("1-Select MinAct");
+  
+  lcd.setCursor(0, 1);
+  lcd.print("2-Skip( default)");
+  delay(3000);
+
+  lcd.clear();
+  short i;
+  for(i=0; i<=5; ++i){
+     lcd.noCursor();
+     delay(300);
+     lcd.cursor();
+     delay(300);
+  }
+}
+
+void welcome(char x0[] , char x1[]){
+  lcd.setCursor(0, 0);
+  lcd.print(x0);
+  lcd.setCursor(0, 1);
+  lcd.print(x1);
+}
+void show_detail(){
+  lcd.setCursor(0, 0);
+  lcd.print("CurH:");
+  lcd.print(humi);
+  lcd.print("% Sts:");
+  lcd.print(sts);
+  //
+  
+  lcd.setCursor(0, 1);
+  lcd.print("MinAct:");
+  lcd.print(MIN_ACT);lcd.print("% ");
+  lcd.print(hh); lcd.print(":"); lcd.print(mm);
+  //MinAct:00% hh:mm
+  
+  
+  //lcd.print("                ");    clsr
+  delay(3000);
+}
+
+void loop() {
+  
+  //Serial.println(humi);                                   // debugging
+  
+  /*
+  welcome(x0, x1);
+  delay(1000);
+  */
+  //test button
+  short i, st;
+  for(i=0; i<10; ++i){
+    st = getButtonStatus();
+    Serial.println(st)
+    delay(200);
+  }
+  
+  //--------------------------
+  show_detail();
+}
